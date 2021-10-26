ad15c1133d3f79cf5a4e85a9d52efdee80aec1c6
xinfo15 <2608443688@qq.com>
2021-10-26 17:07:42 +0800

    分类页面完成列表渲染和简单布局

--- pages/category/main.js
+++ pages/category/main.js
@@ -1,66 +1,37 @@
 // pages/category/main.js
-Page({
+import { request } from '../../request/index.js'
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [],
+    rightContentList: [],
+    cateActiveIdx: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCateList()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 获取分类信息
+  getCateList() {
+    request({
+      url: '/api/public/v1/categories',
+    }).then((res) => {
+      const cateList = res.data.message
 
+      const leftMenuList = cateList.map((item) => item.cat_name)
+      const rightContentList = cateList[0].children
+
+      this.setData({
+        leftMenuList,
+        rightContentList
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
