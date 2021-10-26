258e4ccdbe32e131ad4543dac64acfca60224296
xinfo15 <2608443688@qq.com>
2021-10-27 00:07:08 +0800

    完成分类页面的开发

--- pages/category/main.js
+++ pages/category/main.js
@@ -6,32 +6,76 @@ Page({
    * 页面的初始数据
    */
   data: {
+    cateList: [],
     leftMenuList: [],
     rightContentList: [],
-    cateActiveIdx: 0
+    // 右边内容的滚动高度，用于重新回到顶部
+    rightContentScrollTop: 0,
+    cateActiveIdx: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCateList()
+    this.setCacheForCateList()
+  },
+  // 给cateList数据设置缓存，缓存过期则重新请求接口
+  setCacheForCateList() {
+    let cateList = wx.getStorageSync('cate_list')
+    // 如果没有缓存，就请求接口
+    if (!cateList) {
+      this.getCateList()
+    } else {
+      // 如果有缓存，检查是否过期(30s)
+      // 如果过期
+      if (cateList.time === undefined || Date.now() - cateList.time > 1000 * 30) {
+        this.getCateList()
+      } else {
+        // 如果没过期
+        cateList = cateList.cate_list
+        const leftMenuList = cateList.map((item) => item.cat_name)
+        const rightContentList = cateList[0].children
+
+        this.setData({
+          leftMenuList,
+          rightContentList,
+          cateList,
+        })
+      }
+    }
   },
 
   // 获取分类信息
-  getCateList() {
-    request({
+  async getCateList() {
+    const {
+      data: { message: cateList },
+    } = await request({
       url: '/api/public/v1/categories',
-    }).then((res) => {
-      const cateList = res.data.message
+    })
 
-      const leftMenuList = cateList.map((item) => item.cat_name)
-      const rightContentList = cateList[0].children
+    // const cateList = data.message
+    // 设置缓存
+    wx.setStorageSync('cate_list', { time: Date.now(), cate_list: cateList })
 
-      this.setData({
-        leftMenuList,
-        rightContentList
-      })
+    const leftMenuList = cateList.map((item) => item.cat_name)
+    const rightContentList = cateList[0].children
+
+    this.setData({
+      leftMenuList,
+      rightContentList,
+      cateList,
+    })
+  },
+
+  // 切换菜单高亮显示
+  changeCateActive(e) {
+    const { menuIdx } = e.currentTarget.dataset
+
+    this.setData({
+      cateActiveIdx: menuIdx,
+      rightContentList: this.data.cateList[menuIdx].children,
+      rightContentScrollTop: 0,
     })
   },
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,29 @@
 // pages/goods_list/index.js
-Page({
+import { request } from '../../request/index.js'
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (options.cid) this.getGoodsList(options.cid)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async getGoodsList(cid) {
+    const {
+      data: { message: goodsList },
+    } = await request({
+      url: '/api/public/v1/goods/search',
+      params: {
+        cid,
+      },
+    })
+    console.log(goodsList)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
